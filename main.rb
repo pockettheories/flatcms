11e325136c1387806999a491c5bb6bd6cdd71ac6
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2024-08-22 09:38:15 +0400

    Added layout header and footer to the page rendering

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 
 def process_page(name)
-  ERB.new(File.read("pages/#{name}.erb")).result(binding)
+  (ERB.new(File.read("layout/header.erb")).result(binding)) + \
+  (ERB.new(File.read("pages/#{name}.erb")).result(binding)) + \
+  (ERB.new(File.read("layout/footer.erb")).result(binding))
 end
 
 get '/' do
